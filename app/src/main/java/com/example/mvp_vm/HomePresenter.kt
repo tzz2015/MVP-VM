b5b70627e324a4071a44116cdceb0b82ccbe2dab
tzz2015 <1374329135@qq.com>
2019-06-03 14:04:08 +0800

    弱引用

--- app/src/main/java/com/example/mvp_vm/HomePresenter.kt
+++ app/src/main/java/com/example/mvp_vm/HomePresenter.kt
@@ -15,17 +15,18 @@ class HomePresenter(mView: HomeView, mViewModel: HomeViewModel, owner: Lifecycle
     BasePresenter<HomeView, HomeViewModel>(mView, mViewModel, owner) {
 
     override fun start(owner: LifecycleOwner) {
-        mViewModel?.textLiveData!!.observe(owner, Observer {
+        getViewModel()?.textLiveData?.observe(owner, Observer {
             it?.let { data ->
                 Log.e("HomePresenter", data.text)
             }
         })
+
     }
 
     /**
      * 点击按钮
      */
     fun clickChange() {
-        mViewModel?.getTextData()
+        getViewModel()?.getTextData()
     }
 }
--- app/src/main/java/com/example/mvp_vm/base/BasePresenter.kt
+++ app/src/main/java/com/example/mvp_vm/base/BasePresenter.kt
@@ -2,6 +2,7 @@ package com.example.mvp_vm.base
 
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.Observer
+import java.lang.ref.WeakReference
 
 /**
 16 * @ClassName: BasePresenter
@@ -10,26 +11,44 @@ import android.arch.lifecycle.Observer
 19 * @Date: 2019-05-27 16:46
 20 */
 abstract class BasePresenter<V : BaseView, VM : BaseViewModel>(
-    var mView: V?,
-    var mViewModel: VM?,
+    mView: V?,
+    mViewModel: VM?,
     owner: LifecycleOwner
 ) {
+    private var mViewRef: WeakReference<V>? = WeakReference<V>(mView)
+    private var mViewModelRef: WeakReference<VM>? = WeakReference<VM>(mViewModel)
+
     init {
         this.initCommon(owner)
         this.start(owner)
     }
 
     /**
+     * 获取View
+     */
+    fun getView(): V? {
+        return mViewRef?.get()
+    }
+
+    /**
+     * 获取ViewModel
+     */
+    fun getViewModel(): VM? {
+        return mViewModelRef?.get()
+    }
+
+
+    /**
      * 统一显示加载框
      */
     private fun initCommon(owner: LifecycleOwner) {
-        mViewModel?.loadLiveData?.observe(owner, Observer {
+        getViewModel()?.loadLiveData?.observe(owner, Observer {
             when (it) {
                 true -> {
-                    mView?.showLoading()
+                    getView()?.showLoading()
                 }
                 else -> {
-                    mView?.hideLoading()
+                    getView()?.hideLoading()
                 }
             }
         })
@@ -38,9 +57,10 @@ abstract class BasePresenter<V : BaseView, VM : BaseViewModel>(
     protected abstract fun start(owner: LifecycleOwner)
 
     fun onDestroy() {
-        mView = null
-        mViewModel?.onCleared()
-        mViewModel = null
+        mViewRef?.clear()
+        mViewModelRef?.clear()
+        mViewRef = null
+        mViewModelRef = null
     }
 }
 
