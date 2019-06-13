c0a7403deedbb7987e57f2e37b81981e6345e4c6
tzz2015 <1374329135@qq.com>
2019-06-13 17:41:49 +0800

    跳转第三方APP

--- app/src/main/java/com/example/mvp_vm/WebActivity.kt
+++ app/src/main/java/com/example/mvp_vm/WebActivity.kt
@@ -1,7 +1,10 @@
 package com.example.mvp_vm
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : AppCompatActivity() {
@@ -10,5 +13,22 @@ class WebActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
         vb.loadUrl("file:///android_asset/SchemaLaunch.html")
+        initView()
+    }
+
+    private fun initView() {
+        bt_jump.setOnClickListener {
+            try {
+                var url = "fubei://platformapi/startapp?appId=%s&action=%s"
+                val jumpUrl = String.format(url, input_app_id.text.toString(), input_action.toString())
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(jumpUrl))
+                startActivity(intent)
+            } catch (e: Exception) {
+                Log.e(javaClass.simpleName, e.message)
+            }
+
+        }
+
+
     }
 }
