ebeceb5884e232fbb85ec368b6dcbd3bcfe48a7e
tzz2015 <1374329135@qq.com>
2019-07-02 20:16:52 +0800

    淘宝跳转

--- app/src/main/java/com/example/mvp_vm/activity/WebActivity.kt
+++ app/src/main/java/com/example/mvp_vm/activity/WebActivity.kt
@@ -1,10 +1,16 @@
 package com.example.mvp_vm.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
+import android.net.http.SslError
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.webkit.SslErrorHandler
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import com.example.mvp_vm.R
 import kotlinx.android.synthetic.main.activity_web.*
 
@@ -13,11 +19,14 @@ class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
-        vb.loadUrl("file:///android_asset/SchemaLaunch.html")
         initView()
+        vb.loadUrl("file:///android_asset/SchemaLaunch.html")
+//        vb.loadUrl("https://www.baidu.com")
+
     }
 
     private fun initView() {
+        initWebClient()
         bt_jump.setOnClickListener {
             try {
                 var url = "fubei://platformapi/startapp?appId=%s&action=%s"
@@ -31,4 +40,42 @@ class WebActivity : AppCompatActivity() {
 
         }
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebClient() {
+        val webSetting = vb.settings
+        webSetting.javaScriptEnabled = true
+        vb.webViewClient = object : WebViewClient() {
+
+
+            override fun onReceivedSslError(view: WebView, handler: SslErrorHandler, error: SslError) {
+                handler.proceed()
+                super.onReceivedSslError(view, handler, error)
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                Log.e("url", url)
+                if (url.startsWith("http") || url.startsWith("https")) {
+                    // 返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                    view.loadUrl(url)
+                    return true
+                }
+                if (url.startsWith("taobao://")) {
+                    try {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                        startActivity(intent)
+                        return true
+                    } catch (e: Exception) {
+                        Toast.makeText(this@WebActivity, "请安装淘宝", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+                return super.shouldOverrideUrlLoading(view, url)
+            }
+
+        }
+    }
 }
+
+
